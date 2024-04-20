69017c5df797b58ecc88c61b0b753a2f967f0371
carlin1981 <141785002+carlin1981@users.noreply.github.com>
2024-04-20 20:25:50 -0300

    a

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DisciplinaComponent } from "./components/disciplina/disciplina.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, DisciplinaComponent]
 })
 export class AppComponent {
   title = 'projetointegrador-app';
---
+++ src/app/components/disciplina/disciplina.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-disciplina',
+  standalone: true,
+  imports: [],
+  templateUrl: './disciplina.component.html',
+  styleUrl: './disciplina.component.css'
+})
+export class DisciplinaComponent {
+
+}
